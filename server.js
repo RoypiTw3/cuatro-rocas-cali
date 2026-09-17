78785b53604836fc8d5313e42d1ef01268c4fde1
RoypiTw3 <zroypi@gmail.com>
2026-09-16 20:01:53 -0500

    feat(branding): add official 4 Rocas logo and favicon

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ const MIME_TYPES = {
   '.json': 'application/json',
   '.png': 'image/png',
   '.jpg': 'image/jpeg',
-  '.svg': 'image/svg+xml'
+  '.svg': 'image/svg+xml',
+  '.ico': 'image/x-icon'
 };
 
 const server = http.createServer((req, res) => {
